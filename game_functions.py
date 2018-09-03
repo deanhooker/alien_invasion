396de47a56752dea7cf6db9c94aa32e88bbbd9df
Dean Hooker <deanhooker91@gmail.com>
2018-09-03 16:57:15 -0600

    ship moves left and right

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,23 @@ import sys
 
 import pygame
 
-def check_events():
+def check_events(ship):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                ship.moving_left = False
 
 def update_screen(ai_settings, screen, ship):
     """Update images on the screen and flip to the new screen."""
@@ -16,4 +28,3 @@ def update_screen(ai_settings, screen, ship):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
-    
